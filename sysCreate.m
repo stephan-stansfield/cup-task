cad655bd2f8b56b205297b14ab466b41bbe83238
StephanStansfield <StephanStansfield@users.noreply.github.com>
2023-02-02 13:42:20 -0500

    Update system equations for multi-mode + feedforward to take force input U(t) = 1/K*F(t) + B/K*x_dot_des + x_des instead of desired acceleration input.

--- sysCreate.m
+++ sysCreate.m
@@ -14,52 +14,41 @@ function [sys,sysRigid,Td,Td1,Td2,zeta,zeta1,zeta2,overdamped] = ...
         % Controller includes feedforward force input term
         if forwardF
             
-            A=[    0       0             0            1        0   0 ;      % *[x_des x theta x_des_dot x_dot theta_dot]'   
-                   0       0             0            0        1   0 ;
-                   0       0             0            0        0   1 ;        
-                   0       0             0            0        0   0 ;
-                  k/M    -k/M          m*g/M         b/M    -b/M   0 ;
-               -k/(l*M) k/(l*M) -(g*(m+M))/(l*M)  -b/(l*M) b/(l*M) 0 ];
+            A=[    0           0           1    0 ;                         % *[x theta x_dot theta_dot]'   
+                   0           0           0    1 ;
+                -k/M         m*g/M       -b/M   0 ;
+               k/(l*M) -(g*(m+M))/(l*M) b/(l*M) 0 ];
 
-            % Single input "u" is desired acceleration
-            B=[      0      ;                                               % *x_des_dotdot
-                     0      ;
-                     0      ;
-                     1      ;
-                  (m+M)/M   ;
-               -(m+M)/(l*M) ];
+            % Input U(t) includes feedforward force and half of impedance
+            % force
+            B=[     0  ;                                                    % * U(t)
+                    0  ;
+                   K/m ;
+                -K/(l*M) ];
 
-            % 5 outputs: cart position, ball angle, cart vel, & ball
+            % 4 outputs: cart position, ball angle, cart vel, & ball
             % angular velocity. Ball angle converted from radians to deg.
-            % 5th output is xdes for pendulum lock code to fully "hand off"
-            % the state between simulations when pendulum is released.
-            C=[0      1     0      0    0     0;                            % *[x_des, x, theta, x_des_dot, x_dot, theta_dot]'
-               0      0 360/(2*pi) 0    0     0;
-               0      0     0      0    1     0;
-               0      0     0      0    0 360/(2*pi);
-               1      0     0      0    0     0];
+            C=[1     0      0    0     0;                                   % *[x theta x_dot theta_dot]'  
+               0 360/(2*pi) 0    0     0;
+               0     0      0    1     0;
+               0     0      0    0 360/(2*pi)];
 
-            D=[0;                                                           % *x_des_dotdot
-               0;
+            D=[0;
                0;
                0;
                0];
 
-            states={'x_{des}' 'x' 'theta' 'x_{dot}_{des}' 'x_{dot}' 'theta_{dot}'};
+            states={'x' 'theta' 'x_{dot}' 'theta_{dot}'};
             
-            Arigid = [     0       0     0    1        0    0;              % *[x_des, x, theta, x_des_dot, x_dot, theta_dot]'
-                           0       0     0    0        1    0;
-                           0       0     0    0        0    1;
-                           0       0     0    0        0    0;
-                        k/(m+M) -k/(m+M) 0 b/(m+M) -b/(m+M) 0;
-                           0       0     0    0        0    0];
+            Arigid = [     0     0     1    0 ;                             % *[x theta x_dot theta_dot]'   
+                           0     0     0    1 ;
+                        -k/(m+M) 0 -b/(m+M) 0 ;
+                           0     0     0    0 ];
                         
-           Brigid = [0;                                                     % *x_des_dotdot
-                     0;
-                     0;
-                     1;
-                     1;
-                     0];
+           Brigid = [  0;                                                   % * U(t)
+                       0;
+                     K/(m+M);
+                       0];
 
         % No feedforward force input term; forwardF = false
         else
